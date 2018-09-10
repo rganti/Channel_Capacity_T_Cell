9457167f8f54b2e52e01d8552feaec47b63f42af
ramanganti <rg468@cam.ac.uk>
2018-09-10 11:36:23 -0400

    "script for plotting histograms since expensive to do on jupyter."

---
+++ plot_final_histograms.py
@@ -0,0 +1,38 @@
+import argparse
+
+import matplotlib.pyplot as plt
+
+from compute_ic import InformationCapacity
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Plot histograms for certain steps",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--steps', dest='steps', action='store', type=int, default=8,
+                        help="number of KP steps.")
+    parser.add_argument('--fb', dest='fb', action='store', type=float)
+
+    args = parser.parse_args()
+    steps = args.steps
+    lf = 30
+
+    if steps > 8:
+        if args.fb:
+            file_path = "{0}_step_k_pos_{1}/".format(steps, args.fb)
+        else:
+            file_path = "{0}_step/".format(steps)
+    else:
+        file_path = "{0}_step/".format(steps)
+
+    ic_lf = InformationCapacity(foreign_directory=file_path + "Ls_Lf_{0}/".format(lf),
+                                self_directory=file_path + "Ls/",
+                                limiting="self")
+    ic_lf.plot_cn()
+    ic_lf.plot_dn()
+    plt.legend()
+
+    print("[L_f] = 30")
+    plt.xlim(0, 200)
+
+    plt.title("{0} Steps: IC = {1}".format(steps, ic_lf.calculate_ic()))
+
+    plt.savefig(file_path + "{0}_step.pdf".format(steps), format="pdf")
