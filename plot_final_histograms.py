d8b439183e4b3336bbc171666906cfc2d861abd8
ramanganti <rg468@cam.ac.uk>
2018-09-18 10:45:26 -0400

    "python script for plotting histograms."

--- plot_final_histograms.py
+++ plot_final_histograms.py
@@ -10,16 +10,19 @@ if __name__ == "__main__":
     parser.add_argument('--steps', dest='steps', action='store', type=int, default=8,
                         help="number of KP steps.")
     parser.add_argument('--fb', dest='fb', action='store', type=float)
+    parser.add_argument('--d', dest='d', action='store', type=str)
 
     args = parser.parse_args()
     steps = args.steps
     lf = 30
 
-    if steps > 8:
-        if args.fb:
-            file_path = "{0}_step_k_pos_{1}/".format(steps, args.fb)
-        else:
-            file_path = "{0}_step/".format(steps)
+    # if steps > 8:
+    #     if args.fb:
+    #         file_path = "{0}_step_k_pos_{1}/".format(steps, args.fb)
+    #     else:
+    #         file_path = "{0}_step/".format(steps)
+    if args.d:
+        file_path = args.d
     else:
         file_path = "{0}_step/".format(steps)
 
@@ -31,8 +34,8 @@ if __name__ == "__main__":
     plt.legend()
 
     print("[L_f] = 30")
-    plt.xlim(0, 200)
+    plt.xlim(0, 10)
 
     plt.title("{0} Steps: IC = {1}".format(steps, ic_lf.calculate_ic()))
 
-    plt.savefig(file_path + "{0}_step.pdf".format(steps), format="pdf")
+    plt.savefig(file_path + "histograms.pdf", format="pdf")
