2bd7ff1cd1eeb25cc21813bd2cabb038735c37b6
ramanganti <rg468@cam.ac.uk>
2018-09-21 18:17:44 -0400

    "made changes to parameter range for parameter tests."

--- ode_parameter_tests.py
+++ ode_parameter_tests.py
@@ -20,27 +20,28 @@ class ParameterTesting(object):
         # self.k_8_1 = np.round(np.logspace(-5.0, -3.5, num=5), 6)
         # self.k_8_2 = np.round(np.logspace(-3.0, -1.5, num=5), 4)
 
-        self.k_8_1 = np.round(np.logspace(-4.0, -3.0, num=3), 6)
-        self.k_8_2 = np.round(np.logspace(-3.5, -2.5, num=3), 4)
+        self.k_8_1 = np.round(np.logspace(-3.0, -2.5, num=10), 6)
+        # self.k_8_2 = np.round(np.logspace(-3.5, -2.5, num=3), 4)
 
         # self.k_8_1 = [0.0003]
         # self.k_8_2 = [0.001]
 
-        self.param_grid = {'k_8_1': self.k_8_1, 'k_8_2': self.k_8_2}
+        # self.param_grid = {'k_8_1': self.k_8_1, 'k_8_2': self.k_8_2}
+        self.param_grid = {'k_8_1': self.k_8_1}
+        # self.param_grid = {}
 
         if self.steps > 8:
-            self.param_grid['k_9_1'] = [
-                0.0003]  # np.round(np.logspace(-5.0, -2, num=5), 6)  # np.linspace(0.00005, 0.0003, 3)
-            self.param_grid['k_9_2'] = [
-                0.0003]  # np.round(np.logspace(-5.0, -3, num=5), 6)  # np.linspace(0.00005, 0.0003, 3)
-            self.param_grid['k_9_3'] = np.round(np.logspace(-1.0, 1, num=5),
-                                                4)  # [100 * i for i in self.param_grid['k_8_2']]
+            self.param_grid['k_9_1'] = np.round(np.logspace(-5.0, -2.5, num=8), 6)  # np.linspace(0.00005, 0.0003, 3)
+            # self.param_grid['k_9_2'] = [0.0003]
+            # np.round(np.logspace(-5.0, -3, num=5), 6)  # np.linspace(0.00005, 0.0003, 3)
+            # self.param_grid['k_9_3'] = np.round(np.logspace(-1.0, 1, num=5), 4)
+            # [100 * i for i in self.param_grid['k_8_2']]
 
         self.grid = ParameterGrid(self.param_grid)
+        print(list(self.grid))
 
         self.simulation_name = "ODE_steps_" + str(self.steps)
-        self.simulation_time = 10
-        self.self_foreign = True
+        self.simulation_time = 6
 
     def generate_qsub(self, self_foreign=False):
 
@@ -51,16 +52,16 @@ class ParameterTesting(object):
         q.write("#PBS -l walltime={1},nodes=1:ppn=4 -N {0}\n\n".format(self.simulation_name,
                                                                        datetime.timedelta(
                                                                            minutes=self.simulation_time)))
-        q.write("cd $PBS_O_WORKDIR\n\n")
+        q.write("cd $PBS_O_WORKDIR\n")
         q.write("echo $PBS_JOBID > job_id\n\n")
 
         if self_foreign:
             q.write(
-                "python ~/SSC_python_modules/pysb_t_cell_network.py --cubic --steps {0} --ls_lf --lf {1}\n".format(
+                "python ~/SSC_python_modules/pysb_t_cell_network.py --steps {0} --ls_lf --lf {1}\n".format(
                     self.steps,
                     self.lf))
         else:
-            q.write("python ~/SSC_python_modules/pysb_t_cell_network.py --cubic --steps {0}".format(self.steps))
+            q.write("python ~/SSC_python_modules/pysb_t_cell_network.py --steps {0}".format(self.steps))
         q.close()
 
     def launch(self):
@@ -116,7 +117,7 @@ class ParameterTesting(object):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Submitting ode calculations as function of steps",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--steps', dest='steps', action='store', type=int, default=8, help="number of KP steps.")
+    parser.add_argument('--steps', dest='steps', action='store', type=int, default=9, help="number of KP steps.")
     parser.add_argument('--run', action='store_true', default=False, help='Flag for submitting simulations.')
 
     args = parser.parse_args()
