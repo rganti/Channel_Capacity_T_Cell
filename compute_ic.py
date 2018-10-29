229eb85ae2ccf1a96fd9cec323fea829e95935b2
ramanganti <rg468@cam.ac.uk>
2018-10-29 14:39:30 -0400

    "fixed capacity binning issue once and for all hopefully."

--- compute_ic.py
+++ compute_ic.py
@@ -36,16 +36,18 @@ class InformationCapacity(object):
             self.self_column = load(self_directory + "column_names")
             self.self_column_names = self.self_column[0].split()
 
-        self.bins = self.calculate_bins(estimator)
+        self.estimator = estimator
+        self.bins = self.calculate_bins()
+        self.p_0_integral = 0
 
-    def calculate_bins(self, estimator):
-        count, self_bins = np.histogram(self.self_output, bins=estimator, density=True)
-        count, foreign_bins = np.histogram(self.foreign_output, bins=estimator, density=True)
+    def calculate_bins(self, num_bins=500):
+        count, self_bins = np.histogram(self.self_output, bins=self.estimator, density=True)
+        count, foreign_bins = np.histogram(self.foreign_output, bins=self.estimator, density=True)
 
-        bin_width = self_bins[1] - self_bins[0]
+        # bins = np.arange(min(self_bins), max(foreign_bins), bin_width)
+        bins = np.linspace(min(self_bins), max(foreign_bins), num=num_bins)
 
-        bins = np.arange(min(self_bins), max(foreign_bins), bin_width)
-
+        # print(str(bins))
         return bins
 
     # def kde_plot(self):
@@ -59,8 +61,8 @@ class InformationCapacity(object):
     #              hist_kws={'edgecolor': 'black'},
     #              kde_kws={'linewidth': 4})
 
-    def count_cn(self):
-        count_cn, bins = np.histogram(self.foreign_output, bins=self.bins, density=True)
+    def count_cn(self, bin_locations):
+        count_cn, bins = np.histogram(self.foreign_output, bins=bin_locations, density=True)
 
         return count_cn
 
@@ -90,8 +92,8 @@ class InformationCapacity(object):
         count, bins_lf, _ = plt.hist(self.foreign_ligand, bins=bins, align='mid', normed=True,
                                      label='P(Lf)')
 
-    def count_dn(self):
-        count_dn, bins = np.histogram(self.self_output, bins=self.bins, density=True)
+    def count_dn(self, bin_locations):
+        count_dn, bins = np.histogram(self.self_output, bins=bin_locations, density=True)
         return count_dn
 
     def dn_mean_iqr(self):
@@ -122,23 +124,38 @@ class InformationCapacity(object):
     #     return kl_cn_dn
 
     def calculate_ic(self):
-        count_cn = self.count_cn()
-        count_dn = self.count_dn()
-        bin_width = self.bins[1] - self.bins[0]
+        number_of_bins = 200
 
-        p_O = 0.5 * (count_cn + count_dn)
+        while self.p_0_integral < 0.99:
+            bins = self.calculate_bins(num_bins=number_of_bins)
+            count_cn = self.count_cn(bins)
+            count_dn = self.count_dn(bins)
+            bin_width = bins[1] - bins[0]
 
-        print("p(0) integral = " + str(np.trapz(p_O, dx=bin_width)))
+            p_O = 0.5 * (count_cn + count_dn)
 
-        term_1_c0 = 0.5 * count_cn * np.nan_to_num(np.log2(count_cn / p_O))
-        term_2_d0 = 0.5 * count_dn * np.nan_to_num(np.log2(count_dn / p_O))
-        C = np.trapz(term_1_c0 + term_2_d0, dx=bin_width)
-        print("C = " + str(C))
+            self.p_0_integral = np.trapz(p_O, dx=bin_width)
+            print("p(O) integral = " + str(self.p_0_integral))
+
+            term_1_c0 = 0.5 * count_cn * np.nan_to_num(np.log2(count_cn / p_O))
+            term_2_d0 = 0.5 * count_dn * np.nan_to_num(np.log2(count_dn / p_O))
+            C = np.trapz(term_1_c0 + term_2_d0, dx=bin_width)
+            print("C = " + str(C))
+
+            if self.p_0_integral == C:
+                print("C == P(O) integral: " + str(self.p_0_integral == C))
+                C = 1.00
+                print("New C " + str(C))
+                break
+
+            number_of_bins += 50
+
         return C
 
     def alternate_calculate_ic(self):
-        count_cn = self.count_cn()
-        count_dn = self.count_dn()
+        bins = self.calculate_bins(num_bins=500)
+        count_cn = self.count_cn(bins)
+        count_dn = self.count_dn(bins)
         bin_width = self.bins[1] - self.bins[0]
 
         p_O = 0.5 * (count_cn + count_dn)
@@ -192,7 +209,7 @@ def check_binning():
 if __name__ == "__main__":
     ic = InformationCapacity()
     ic.calculate_ic()
-    ic.alternate_calculate_ic()
+    # ic.alternate_calculate_ic()
     # check_binning()
     # plt.savefig("output_histograms_test.pdf", format='pdf')
     # ic.plot_histograms()
