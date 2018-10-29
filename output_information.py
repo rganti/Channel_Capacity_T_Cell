5880635c6e14c05d5fa7326da083f24c1063918a
ramanganti <rg468@cam.ac.uk>
2018-10-29 15:43:01 -0400

    "python script for outputting information from parameter tests."

--- output_information.py
+++ output_information.py
@@ -1,23 +1,54 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from compute_ic import InformationCapacity
+
+
+class OneDHistogramInformation(object):
+    def __init__(self, directory="."):
+        self.df = pd.read_csv(directory + "/file_paths", sep='\t', index_col=0)
+        self.ic = []
+        self.kp_lat_1 = []
+        self.file_paths = [directory + "/" + path for path in self.df['file_path'][2:]]
+        self.lf = 30
+
+    def compute_variables(self):
+        count = 0
+        for file_path in self.file_paths:
+            ic = InformationCapacity(foreign_directory=file_path + "/Ls_Lf_{0}/".format(self.lf),
+                                     self_directory=file_path + "/Ls/", limiting="self")
+            C = ic.calculate_ic()
+            np.savetxt(file_path + "/IC", [C], fmt="%f")
+            parameters = pickle.load(open(file_path + "/Ls/parameters.pickle", "rb"))
+
+            self.ic.append(C)
+            self.kp_lat_1.append(parameters['kp_on_lat1'])
+            count += 1
 
 
 class HistogramInformation(object):
     def __init__(self):
         self.df = pd.read_csv("./file_paths", sep='\t', index_col=0)
         self.ic = []
-        self.lf_mean = []
-        self.ls_mean = []
-        self.lf_iqr = []
-        self.ls_iqr = []
+        self.kp_lat_2 = []
+
+        self.file_paths = self.df['file_path'][10:]
 
         self.lf = 30
 
     def compute_variables(self):
-        for file_path in self.df['file_path']:
+        k_on = []
+        kp_2 = []
+        count = 0
+        for file_path in self.file_paths:
             ic = InformationCapacity(foreign_directory=file_path + "/Ls_Lf_{0}/".format(self.lf),
                                      self_directory=file_path + "/Ls/", limiting="self")
+            C = ic.calculate_ic()
+            np.savetxt(file_path + "/IC", [C], fmt="%f")
+            parameters = pickle.load(open(file_path + "/Ls/parameters.pickle", "rb"))
 
             # ic.plot_cn()
             # ic.plot_dn()
@@ -28,15 +59,23 @@ class HistogramInformation(object):
             # plt.savefig(file_path + "/histograms.pdf", format="pdf")
             # plt.close()
 
-            lf_mean, lf_iqr = ic.cn_mean_iqr()
-            self.lf_mean.append(lf_mean)
-            self.lf_iqr.append(lf_iqr)
+            # lf_mean, lf_iqr = ic.cn_mean_iqr()
+            # self.lf_mean.append(lf_mean)
+            # self.lf_iqr.append(lf_iqr)
+            #
+            # ls_mean, ls_iqr = ic.dn_mean_iqr()
+            # self.ls_mean.append(ls_mean)
+            # self.ls_iqr.append(ls_iqr)
 
-            ls_mean, ls_iqr = ic.dn_mean_iqr()
-            self.ls_mean.append(ls_mean)
-            self.ls_iqr.append(ls_iqr)
-
-            self.ic.append(ic.calculate_ic())
+            k_on.append(C)
+            kp_2.append(parameters['kp_on_lat2'])
+            count += 1
+            if count == 5:
+                self.ic.append(k_on)
+                self.kp_lat_2.append(kp_2)
+                kp_2 = []
+                k_on = []
+                count = 0
 
     def modify_df(self):
         self.df['ls_mean'] = self.ls_mean
@@ -51,7 +90,19 @@ class HistogramInformation(object):
 
     def main(self):
         self.compute_variables()
-        self.modify_df()
+
+        plt.imshow(np.array(self.ic), extent=[1.0, 100.0, 5.0, 0.5], aspect=2)
+        plt.xticks(np.array([1.0, 5.0, 10.0, 50.0, 100.0]))
+        plt.yticks(np.linspace(0.5, 5.0, 10))
+
+        plt.ylabel("$k_{on}$")
+        plt.xlabel("reduction")
+
+        plt.colorbar(orientation='vertical')
+
+        plt.savefig("capacity_on_rates.pdf", format="pdf")
+
+        # self.modify_df()
 
 
 if __name__ == "__main__":
