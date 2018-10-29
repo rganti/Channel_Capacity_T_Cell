262cc38a6358de87578e0dca116793ad99223de0
ramanganti <rg468@cam.ac.uk>
2018-10-29 14:41:53 -0400

    "Changed script for running parameter tests. Using now to find parameter regime for latp calculations"

--- ode_parameter_tests.py
+++ ode_parameter_tests.py
@@ -6,42 +6,33 @@ import subprocess
 
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import ParameterGrid
 
-from realistic_network import make_and_cd
+from realistic_network import make_and_cd, BindingParameters
 
 
 class ParameterTesting(object):
     def __init__(self, steps=8):
+        self.binding_constants = BindingParameters()
 
         self.lf = 30
         self.sub_directories = ["Ls", "Ls_Lf_{0}".format(self.lf)]
         self.steps = steps
-        # self.k_8_1 = np.round(np.logspace(-5.0, -3.5, num=5), 6)
-        # self.k_8_2 = np.round(np.logspace(-3.0, -1.5, num=5), 4)
+        #
+        # self.k_8_1 = np.round(np.logspace(-3.0, -2.5, num=10), 6)
+        # self.param_grid = {'k_8_1': self.k_8_1}
+        #
+        # if self.steps > 8:
+        #     self.param_grid['k_9_1'] = np.round(np.logspace(-5.0, -2.5, num=8), 6)  # np.linspace(0.00005, 0.0003, 3)
+        #
+        # self.grid = ParameterGrid(self.param_grid)
+        # print(list(self.grid))
 
-        self.k_8_1 = np.round(np.logspace(-3.0, -2.5, num=10), 6)
-        # self.k_8_2 = np.round(np.logspace(-3.5, -2.5, num=3), 4)
-
-        # self.k_8_1 = [0.0003]
-        # self.k_8_2 = [0.001]
-
-        # self.param_grid = {'k_8_1': self.k_8_1, 'k_8_2': self.k_8_2}
-        self.param_grid = {'k_8_1': self.k_8_1}
-        # self.param_grid = {}
-
-        if self.steps > 8:
-            self.param_grid['k_9_1'] = np.round(np.logspace(-5.0, -2.5, num=8), 6)  # np.linspace(0.00005, 0.0003, 3)
-            # self.param_grid['k_9_2'] = [0.0003]
-            # np.round(np.logspace(-5.0, -3, num=5), 6)  # np.linspace(0.00005, 0.0003, 3)
-            # self.param_grid['k_9_3'] = np.round(np.logspace(-1.0, 1, num=5), 4)
-            # [100 * i for i in self.param_grid['k_8_2']]
-
-        self.grid = ParameterGrid(self.param_grid)
-        print(list(self.grid))
+        self.paths = []
+        self.parameters = []
+        self.home_directory = os.getcwd()
 
         self.simulation_name = "ODE_steps_" + str(self.steps)
-        self.simulation_time = 6
+        self.simulation_time = 10
 
     def generate_qsub(self, self_foreign=False):
 
@@ -49,7 +40,7 @@ class ParameterTesting(object):
         q.write("#PBS -m ae\n")
         q.write("#PBS -q short\n")
         q.write("#PBS -V\n")
-        q.write("#PBS -l walltime={1},nodes=1:ppn=4 -N {0}\n\n".format(self.simulation_name,
+        q.write("#PBS -l walltime={1},nodes=1:ppn=2 -N {0}\n\n".format(self.simulation_name,
                                                                        datetime.timedelta(
                                                                            minutes=self.simulation_time)))
         q.write("cd $PBS_O_WORKDIR\n")
@@ -57,7 +48,7 @@ class ParameterTesting(object):
 
         if self_foreign:
             q.write(
-                "python ~/SSC_python_modules/pysb_t_cell_network.py --steps {0} --ls_lf --lf {1}\n".format(
+                "python ~/SSC_python_modules/pysb_t_cell_network.py --steps {0} --lf {1}\n".format(
                     self.steps,
                     self.lf))
         else:
@@ -84,6 +75,52 @@ class ParameterTesting(object):
             if args.run:
                 self.launch()
             os.chdir(home)
+
+    def create_submit(self, count, param_grid):
+        file_path = "{0}_step_{1}".format(self.steps, count)
+        self.paths.append(file_path)
+        print("param_grid " + str(param_grid))
+        self.parameters.append(str(param_grid))
+
+        make_and_cd(file_path)
+        self.make_launch_simulations(param_grid)
+        os.chdir(self.home_directory)
+
+    def run_parameter_search(self):
+        count = 0
+
+        on_rates = np.linspace(0.5, 5.0, 10)
+        discount = [1.0, 5.0, 10.0, 50.0, 100.0]
+
+        param_grid = {}
+
+        for on_rate in on_rates:
+            param_grid['k_lck_on_RL'] = on_rate / self.binding_constants.initial.lck_0
+            param_grid['k_p_on_R_pmhc'] = on_rate
+            param_grid['k_zap_on_R_pmhc'] = on_rate / self.binding_constants.initial.zap_0
+            param_grid['k_p_on_zap_species'] = on_rate
+            param_grid['k_lat_on_species'] = on_rate / self.binding_constants.initial.lat_0
+            param_grid['kp_on_lat1'] = on_rate
+
+            if self.steps > 7:
+                for i in discount:
+                    param_grid['kp_on_lat2'] = on_rate / i
+                    self.create_submit(count, param_grid)
+
+                    count += 1
+            else:
+                self.create_submit(count, param_grid)
+                count += 1
+
+        df = pd.DataFrame({'file_path': self.paths})
+        df.to_csv("./file_paths", sep='\t')
+
+        df_2 = pd.DataFrame({'file_path': self.paths, 'parameters': self.parameters})
+        df_2.to_csv("./parameters", sep='\t')
+
+        pickle_out = open("parameter_range.pickle", "wb")
+        pickle.dump(param_grid, pickle_out)
+        pickle_out.close()
 
     def run_tests(self):
         paths = []
@@ -124,4 +161,4 @@ if __name__ == "__main__":
 
     p_test = ParameterTesting(steps=args.steps)
 
-    p_test.run_tests()
+    p_test.run_parameter_search()
