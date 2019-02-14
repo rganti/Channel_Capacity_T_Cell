7b33710d776a47229a8c1730db2a0553ec7b6ff8
ramanganti <rg468@cam.ac.uk>
2019-02-14 17:02:12 -0500

    "made changes to HistogramInformation class: added plot capability."

--- output_information.py
+++ output_information.py
@@ -1,3 +1,4 @@
+import argparse
 import pickle
 
 import matplotlib.pyplot as plt
@@ -20,7 +21,7 @@ class OneDHistogramInformation(object):
         for file_path in self.file_paths:
             ic = InformationCapacity(foreign_directory=file_path + "/Ls_Lf_{0}/".format(self.lf),
                                      self_directory=file_path + "/Ls/", limiting="self")
-            C = ic.calculate_ic()
+            C = ic.capacity
             np.savetxt(file_path + "/IC", [C], fmt="%f")
             parameters = pickle.load(open(file_path + "/Ls/parameters.pickle", "rb"))
 
@@ -30,25 +31,48 @@ class OneDHistogramInformation(object):
 
 
 class HistogramInformation(object):
-    def __init__(self, directory=".", lf=30):
+    def __init__(self, directory=".", steps=8, lf=1):
         self.df = pd.read_csv(directory + "/file_paths", sep='\t', index_col=0)
         self.ic = []
-        self.kp_lat_2 = []
+        self.rate_variable = []
+        self.on_rate = []
 
         self.file_paths = [directory + "/" + path for path in self.df['file_path']]
         self.lf = lf
+        self.steps = steps
 
         self.x_array = np.array([1.0, 5.0, 10.0, 50.0, 100.0])
         self.y_array = np.linspace(0.5, 5.0, 10)
 
+    def plot_histograms(self):
+        for file_path in self.file_paths:
+            print("file_path = " + str(file_path))
+            ic = InformationCapacity(foreign_directory=file_path + "/Ls_Lf_{0}/".format(self.lf),
+                                     self_directory=file_path + "/Ls/", limiting="self")
+            C = ic.capacity
+            np.savetxt(file_path + "/IC", [C], fmt="%f")
+
+            ic.plot_cn()
+            ic.plot_dn()
+            plt.legend()
+
+            print("[L_f] = {0}".format(self.lf))
+
+            plt.title("{:.0f} Steps: C = {:.3f}, P_0_integral = {:.3f}".format(self.steps, C,
+                                                                               ic.p0_integral))
+
+            plt.savefig(file_path + "/histograms_lf_{0}.pdf".format(self.lf), format="pdf")
+            plt.close()
+
     def compute_variables(self, rate='kp_on_lat2'):
         k_on = []
-        kp_2 = []
+        capacity = []
+        rate_parameter = []
         count = 0
         for file_path in self.file_paths:
             ic = InformationCapacity(foreign_directory=file_path + "/Ls_Lf_{0}/".format(self.lf),
                                      self_directory=file_path + "/Ls/", limiting="self")
-            C = ic.calculate_ic()
+            C = ic.capacity
             np.savetxt(file_path + "/IC", [C], fmt="%f")
             parameters = pickle.load(open(file_path + "/Ls/parameters.pickle", "rb"))
 
@@ -69,14 +93,19 @@ class HistogramInformation(object):
             # self.ls_mean.append(ls_mean)
             # self.ls_iqr.append(ls_iqr)
 
-            k_on.append(C)
-            kp_2.append(parameters[rate])
+            capacity.append(C)
+            rate_parameter.append(parameters[rate])
+            k_on.append(parameters['kp_on_lat1'])
+
             count += 1
             if count == 5:
-                self.ic.append(k_on)
-                self.kp_lat_2.append(kp_2)
-                kp_2 = []
+                self.ic.append(capacity)
+                self.rate_variable.append(rate_parameter)
+                self.on_rate.append(k_on)
+
                 k_on = []
+                rate_parameter = []
+                capacity = []
                 count = 0
 
     # def modify_df(self):
@@ -106,6 +135,12 @@ class HistogramInformation(object):
 
 
 if __name__ == "__main__":
-    hist_info = HistogramInformation()
+    parser = argparse.ArgumentParser(description="Plot histograms for certain steps",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--steps', dest='steps', action='store', type=int, default=8,
+                        help="number of KP steps.")
+    args = parser.parse_args()
 
-    hist_info.main()
+    hist_info = HistogramInformation(steps=args.steps)
+
+    hist_info.plot_histograms()
