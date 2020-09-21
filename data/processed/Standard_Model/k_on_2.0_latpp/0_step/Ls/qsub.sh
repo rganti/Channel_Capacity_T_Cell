ef4fb5cbcffd2d4824273fe85452a01de5c03e97
Raman <rganti@mit.edu>
2020-09-21 16:56:06 -0400

    removed data files from tracking

--- data/processed/Standard_Model/k_on_2.0_latpp/0_step/Ls/qsub.sh
+++
@@ -1,9 +0,0 @@
-#PBS -m ae
-#PBS -q short
-#PBS -V
-#PBS -l walltime=0:10:00,nodes=1:ppn=2 -N ODE_steps_0
-
-cd $PBS_O_WORKDIR
-echo $PBS_JOBID > job_id
-
-python ~/SSC_python_modules/pysb_t_cell_network.py --steps 0
