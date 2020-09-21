ecba33d68bb273e0460f31dfcb1a7891412d1c58
Raman <rganti@mit.edu>
2020-09-21 17:05:33 -0400

    All data to generate figures in main text

---
+++ data/processed/New_Negative_Fb_Param_Search/Lf_10/8_step_12/Ls_Lf_10/qsub.sh
@@ -0,0 +1,9 @@
+#PBS -m ae
+#PBS -q short
+#PBS -V
+#PBS -l walltime=0:10:00,nodes=1:ppn=2 -N ODE_steps_8
+
+cd $PBS_O_WORKDIR
+echo $PBS_JOBID > job_id
+
+python ~/SSC_python_modules/pysb_t_cell_network.py --steps 8 --lf 10
